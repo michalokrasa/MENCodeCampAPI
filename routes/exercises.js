967ad58cdd45de4c2712b6f446eefc8aaf7091b0
BalboBigguns <micholyea@gmail.com>
2020-05-20 21:22:06 +0200

    All requirements met

--- routes/exercises.js
+++ routes/exercises.js
@@ -14,17 +14,17 @@ router.post('/new-user', async (req, res) => {
     });
 
     user.save()
-        .then(({ username, _id }) => {
-            res.json({ username, _id });
-        })
-        .catch(err => {
-            if (err.code === USERNAME_TAKEN) {
-                res.send('username already taken');
-            }
-            else {
-                res.json({ message: err });
-            }
-        });
+    .then(({ username, _id }) => {
+        res.json({ username, _id });
+    })
+    .catch(err => {
+        if (err.code === USERNAME_TAKEN) {
+            res.send('username already taken');
+        }
+        else {
+            res.json({ message: err });
+        }
+    });
 });
 
 // Add new exercise
@@ -62,30 +62,90 @@ router.post('/add', async (req, res, next) => {
     }
 
     exercise.save()
-        .then(result => {
-            res.json({ 
-                username: fetchedName, 
-                description, duration, 
-                _id: userId, 
-                date: result.date.toDateString() 
-            });
-            console.log(result);
-        })
-        .catch(err => {
-            console.log(err);
-            res.send(err);
+    .then(result => {
+        res.json({ 
+            username: fetchedName, 
+            description, duration, 
+            _id: userId, 
+            date: result.date.toDateString() 
         });
+        console.log(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.send(err);
+    });
 });
 
 // Get all the users
 router.get('/users', (req, res) => {
     User.find()
-        .then(users => {
-            res.json(users);
-        }) 
+    .then(users => {
+        res.json(users);
+    }) 
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+// Get logs
+router.get('/log', (req, res) => {
+    let { userId, from, to, limit } = req.query;
+
+    if (!userId) {
+        res.send('unknown userId');
+        return
+    }
+
+    from = new Date(from);
+    to = new Date(to);
+
+    if (limit && limit < 1) {
+        limit = undefined;
+    }
+    
+
+
+    User.findById(userId).lean()
+    .then(user => {
+        Exercise.find({ userId }).lean()
+        .then( exercises => {
+            user.count = exercises.length;
+            user.log = exercises
+            .filter(e => {
+                if (!isNaN(from) && e.date < from) {
+                    return false;
+                }
+                if (!isNaN(to) && e.date >= to) {
+                    return false;
+                }
+                return true;
+            })
+            .map(e => { 
+                const dateString = e.date.toDateString();
+                return {
+                    description: e.description, 
+                    duration: e.duration,
+                    date: dateString
+                }
+            })
+            
+            if (limit) {
+                user.log = user.log.slice(0, limit);
+            }
+
+            delete user.__v;
+            res.json(user);
+        })
         .catch(err => {
-            res.json(err);
+            console.log(err);
+            res.send(err.message);
         });
+    })
+    .catch(err => {
+        console.log(err);
+        res.send('unknown userId');
+    });
 });
 
 module.exports = router;
